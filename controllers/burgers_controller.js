d36f654928ad34b4475e9d98121266231a764fef
Wendy Wigg <wiggwendy@gmail.com>
2019-06-02 16:37:19 -0700

    updated controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var router = express.Router();
+// import the model (burger.js) to use its database function
+var burger = require("../models/burger.js");
+
+// create route for get all burgers
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+// create route for posting new burger
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+// create route for updating (devouring) burger
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burgers.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            // if no rows were changed then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// export routes for server.js to use
+module.exports = router;
