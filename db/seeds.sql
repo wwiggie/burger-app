5b1e0bb8324af42fcf2a0850cf7c3f12af63f8ce
Wendy Wigg <wiggwendy@gmail.com>
2019-06-02 16:01:23 -0700

    completed schema & seed sql files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Crispy Chicken Burger", false);
