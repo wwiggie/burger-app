b9539e25ce2bb868bf96a9b72cb1e90a7b73681f
Wendy Wigg <wiggwendy@gmail.com>
2019-06-02 16:11:48 -0700

    updated connection.js

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// set up MySQL connection
+var mysql = require("mysql");
+// set up secure mysql login info
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+// make connection
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// export connection
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+
+// start our server so it can begin listening to client requests
+app.listen(PORT, function() {
+    // log (server-side) when our server has started
+    console.log("server listening on: http://localhost:" + PORT);
+});
