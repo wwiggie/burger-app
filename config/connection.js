ce949ad4a81114ce5508ba8ce3bffb3289fe48b0
Wendy Wigg <wiggwendy@gmail.com>
2019-06-03 12:08:36 -0700

    added JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,19 @@ var mysql = require("mysql");
 // set up secure mysql login info
 require("dotenv").config();
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    });
+};
+
+/*
 var connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: 3306,
@@ -10,6 +23,7 @@ var connection = mysql.createConnection({
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
 });
+*/
 
 // make connection
 connection.connect(function(err) {
