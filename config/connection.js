e39aecb08064f66c2c2a7264b2cb21f257889670
Wendy Wigg <wiggwendy@gmail.com>
2019-06-03 15:10:24 -0700

    trying to deploy to heroku round 2

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,10 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        port: 3306,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME
+        host: 'localhost',
+        user: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db'
     });
 };
 
